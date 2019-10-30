1c62629dc4fbf0a5dddafe06bd86f83d00a18d75
Mason Reynolds <masonreynolds65@gmail.com>
2019-10-30 11:19:59 -0400

    Added user input for file name

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using graph_algorithms.logging;
+using graph_algorithms.data_structures;
 
 namespace graph_algorithms
 {
@@ -7,8 +9,33 @@ namespace graph_algorithms
     {
         static void Main(string[] args)
         {
+            bool invalidInput = false;
+
+            do
+            {
+                try
+                {
+                    string fileName = GetUserInput();
+                    Graph<string>.BuildGraphFromFile(fileName);
+                }
+                catch(FileNotFoundException e)
+                {
+                    Logger.WriteLine("File could not be found. Please try again. ");
+                    invalidInput = true;
+                }
+            }
+            while(invalidInput);
+
+            
+
             Console.WriteLine("Press enter to exit....");
             Console.ReadLine();
+        }
+
+        static string GetUserInput()
+        {
+            Logger.WriteLine("Which file would you like to use to create a graph? ");
+            return Logger.ReadLine();
         }
     }
 }
